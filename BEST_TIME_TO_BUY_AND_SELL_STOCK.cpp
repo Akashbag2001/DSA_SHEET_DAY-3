fbcfe873936c3d4e404485fec2ce7a883839f4c5
Akash Bag <78099533+Akashbag2001@users.noreply.github.com>
2022-06-06 13:39:26 +0530

    Create BEST_TIME_TO_BUY_AND_SELL_STOCK.cpp

---
+++ BEST_TIME_TO_BUY_AND_SELL_STOCK.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int n = prices.size();
+    int min_price=INT_MAX;
+    int max_pro= 0;
+    
+    for(int i=0;i<n;i++){
+       min_price=min(min_price,prices[i]);
+       max_pro= max(max_pro,(prices[i]-min_price)); 
+    } return max_pro;
+}
